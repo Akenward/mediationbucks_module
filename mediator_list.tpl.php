f60f4fa10a898635a76cea240f1118e7f6a01b79
Andy Kenward <andy@kenruff.co.uk>
2016-02-15 23:25:14 +0000

    Including all module fies

---
+++ mediator_list.tpl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Default theme implementation to display a mediator list block.
+ */
+ ?>
+ 
+  <p><h2> <?php print $variables['subject']; ?> </h2></p>
+ 
+  <!-- Set up an empty div for jQuery to write the list of mediators. -->
+  <div id="mediator-list"> </div>
+	
+  <!-- Set up an empty div for jQuery to write notes for the selected mediator. -->
+  <br> <div id="mediator-notes"> </div>
+  
